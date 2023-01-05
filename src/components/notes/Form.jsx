e3241b564f441f0984a7b6b5f2ef1d6a5818ffb9
Rohanjai <jairohan2912@gmail.com>
2023-01-05 22:52:21 +0530

    Performance Improvements

--- src/components/notes/Form.jsx
+++ src/components/notes/Form.jsx
@@ -1,86 +1,88 @@
-import { useState, useRef, useContext } from 'react';
+import { useState, useRef, useContext, useCallback } from "react";
 
-import { Box, TextField, ClickAwayListener } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import { v4 as uuid } from 'uuid';
+import { Box, TextField, ClickAwayListener } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { v4 as uuid } from "uuid";
 
-import { DataContext } from '../../context/DataProvider';
+import { DataContext } from "../../context/DataProvider";
 
 const Container = styled(Box)`
-    display: flex;
-    flex-direction: column;
-    margin: auto;
-    box-shadow: 0 1px 2px 0 rgb(60 64 67 / 30%), 0 2px 6px 2px rgb(60 64 67 / 15%);
-    border-color: #e0e0e0;
-    width: 600px;
-    border-radius: 8px;
-    min-height: 30px;
-    padding: 10px 15px;
-`
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  box-shadow: 0 1px 2px 0 rgb(60 64 67 / 30%), 0 2px 6px 2px rgb(60 64 67 / 15%);
+  border-color: #e0e0e0;
+  width: 600px;
+  border-radius: 8px;
+  min-height: 30px;
+  padding: 10px 15px;
+`;
 
 const note = {
-    id: '',
-    heading: '',
-    text: ''
-}
+  id: "",
+  heading: "",
+  text: "",
+};
 
 const Form = () => {
+  const [showTextField, setShowTextField] = useState(false);
+  const [addNote, setAddNote] = useState({ ...note, id: uuid() });
 
-    const [showTextField, setShowTextField] = useState(false);
-    const [addNote, setAddNote] = useState({ ...note, id: uuid() });
+  const { setNotes } = useContext(DataContext);
 
-    const { setNotes } = useContext(DataContext);
-    
-    const containerRef = useRef();
+  const containerRef = useRef();
 
-    const handleClickAway = () => {
-        setShowTextField(false);
-        containerRef.current.style.minheight = '30px'
-        setAddNote({ ...note, id: uuid() });
+  const handleClickAway = useCallback(() => {
+    setShowTextField(false);
+    containerRef.current.style.minheight = "30px";
+    setAddNote({ ...note, id: uuid() });
 
-        if (addNote.heading || addNote.text) {
-            setNotes(prevArr => [addNote, ...prevArr])
-        }
+    if (addNote.heading || addNote.text) {
+      setNotes((prevArr) => [addNote, ...prevArr]);
     }
-    
-    const onTextAreaClick = () => {
-        setShowTextField(true);
-        containerRef.current.style.minheight = '70px'
-    }
+  }, [setShowTextField, setAddNote, addNote, setNotes]);
 
-    const onTextChange = (e) => {
-        let changedNote = { ...addNote, [e.target.name]: e.target.value };
-        setAddNote(changedNote);
-    }
+  const onTextAreaClick = useCallback(() => {
+    setShowTextField(true);
+    containerRef.current.style.minheight = "70px";
+  }, [setShowTextField]);
 
-    return (
-        <ClickAwayListener onClickAway={handleClickAway}>
-            <Container ref={containerRef}>
-                {   showTextField && 
-                    <TextField 
-                        placeholder="Title"
-                        variant="standard"
-                        InputProps={{ disableUnderline: true }}
-                        style={{ marginBottom: 10 }}
-                        onChange={(e) => onTextChange(e)}
-                        name='heading'
-                        value={addNote.heading}
-                    />
-                }
-                <TextField
-                    placeholder="Take a note..."
-                    multiline
-                    maxRows={Infinity}
-                    variant="standard"
-                    InputProps={{ disableUnderline: true }}
-                    onClick={onTextAreaClick}
-                    onChange={(e) => onTextChange(e)}
-                    name='text'
-                    value={addNote.text}
-                />
-            </Container>
-        </ClickAwayListener>
-    )
-}
+  const onTextChange = useCallback(
+    (e) => {
+      let changedNote = { ...addNote, [e.target.name]: e.target.value };
+      setAddNote(changedNote);
+    },
+    [setAddNote, addNote]
+  );
+
+  return (
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <Container ref={containerRef}>
+        {showTextField && (
+          <TextField
+            placeholder="Title"
+            variant="standard"
+            InputProps={{ disableUnderline: true }}
+            style={{ marginBottom: 10 }}
+            onChange={(e) => onTextChange(e)}
+            name="heading"
+            value={addNote.heading}
+          />
+        )}
+        <TextField
+          placeholder="Take a note..."
+          multiline
+          maxRows={Infinity}
+          variant="standard"
+          InputProps={{ disableUnderline: true }}
+          onClick={onTextAreaClick}
+          onChange={(e) => onTextChange(e)}
+          name="text"
+          value={addNote.text}
+        />
+      </Container>
+    </ClickAwayListener>
+  );
+};
 
 export default Form;
