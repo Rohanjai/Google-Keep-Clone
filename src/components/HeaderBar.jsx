189bcbc8bce1aae9001b0cd2b3121c2abb004945
Rohanjai <jairohan2912@gmail.com>
2023-01-05 18:30:36 +0530

    Initial Commit

---
+++ src/components/HeaderBar.jsx
@@ -0,0 +1,43 @@
+import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import { Menu } from "@mui/icons-material";
+import { styled } from "@mui/material/styles";
+import { Link } from "react-router-dom";
+
+const Header = styled(AppBar)`
+  z-index: 1201;
+  background: #fff;
+  height: 70px;
+  box-shadow: inset 0 -1px 0 0 #dadce0;
+`;
+
+const Heading = styled(Typography)`
+  color: #5f6368;
+  font-size: 24px;
+  margin-left: 25px;
+`;
+
+const HeaderBar = ({ open, handleDrawer }) => {
+  const logo =
+    "https://seeklogo.com/images/G/google-keep-logo-0BC92EBBBD-seeklogo.com.png";
+
+  return (
+    <Header open={open}>
+      <Toolbar>
+        <IconButton
+          onClick={() => handleDrawer()}
+          sx={{ marginRight: "20px" }}
+          edge="start"
+        >
+          <Menu />
+        </IconButton>
+        <Link to="/">
+          <img src={logo} alt="logo" style={{ width: 30 }} />
+        </Link>
+
+        <Heading>Keep</Heading>
+      </Toolbar>
+    </Header>
+  );
+};
+
+export default HeaderBar;
