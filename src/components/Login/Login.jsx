ce1f76f0d1c04c8a7929554d359b404d98420a63
Rohanjai <jairohan2912@gmail.com>
2023-01-08 20:46:35 +0530

    Code Clean Up

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,18 @@
 import { React, useCallback } from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
+import {
+  Grid,
+  Box,
+  CssBaseline,
+  TextField,
+  Avatar,
+  Button,
+  Typography,
+  Container,
+  Checkbox,
+  FormControlLabel,
+} from "@mui/material/";
 import { Link } from "react-router-dom";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme();
