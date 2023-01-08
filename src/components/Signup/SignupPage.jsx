818f1a0ce056b7ab2ed48afd58c3246c364bc8cf
Rohanjai <jairohan2912@gmail.com>
2023-01-08 20:43:44 +0530

    Code Clean Up

--- src/components/Signup/SignupPage.jsx
+++ src/components/Signup/SignupPage.jsx
@@ -1,14 +1,16 @@
 import { React, useCallback } from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
 import { Link } from "react-router-dom";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
+import {
+  Grid,
+  Box,
+  CssBaseline,
+  TextField,
+  Avatar,
+  Button,
+  Typography,
+  Container,
+} from "@mui/material/";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme();
