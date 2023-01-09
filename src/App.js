31cab568893c0146845209ba8d9b98a78df10ee6
Rohanjai <jairohan2912@gmail.com>
2023-01-09 13:29:28 +0530

    Code Cleanup Done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import Home from "./components/Home";
 import DataProvider from "./context/DataProvider";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
-  const darkTheme = createTheme({
-    palette: {
-      mode: "light",
-    },
-  });
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <DataProvider>
-        <Home />
-      </DataProvider>
-    </ThemeProvider>
+    <DataProvider>
+      <Home />
+    </DataProvider>
   );
 }
 
